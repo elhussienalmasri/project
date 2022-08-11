7ed4bb068aeaacce5a971b835c6c59eb1e6597fb
elhussein <90080237+elhussienalmasri@users.noreply.github.com>
2022-08-12 00:20:58 +0200

    deployr

--- client/src/components/nav/Navbar.js
+++ client/src/components/nav/Navbar.js
@@ -81,7 +81,7 @@ function Navbar() {
                 className={item.sName}
                 onClick={() => setSidebar(false)}
               >
-                <Link to={item.path} onClick={item.click}>
+                <Link onClick={item.click} to={item.path} >
                   {item.icon}
                   <span>{item.title}</span>
                 </Link>
